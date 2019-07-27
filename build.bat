b8bcad8645d2f6ba971af20fc54b92d4e2403138
Kaleb Klein <klein.jae@gmail.com>
2019-07-27 16:26:34 -0400

    Removed nuget.exe and added download url link instead
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- build.bat
+++ build.bat
@@ -6,6 +6,8 @@ set PATH=%VS%;%UTIL%;%PATH%;
 
 set FLAGS=-p:Configuration=Release -p:OutputPath="%CWD%\bin"
 
+if not exist util\nuget.exe goto download
+
 nuget restore
 nuget restore il-repack
 
@@ -13,3 +15,12 @@ msbuild il-repack\ILRepack\ILRepack.csproj %FLAGS%
 msbuild DSCM.sln -m -nr:false %FLAGS%
 
 %CWD%\bin\ILRepack.exe /log:merge.log /verbose /out:%CWD%\dist\DSCM.exe %CWD%\bin\DSCM.exe %CWD%\bin\CommonMark.dll
+goto end
+
+:download
+echo NuGet is needed to get project packages
+echo Click the link in the util folder to download it
+echo.
+pause
+
+:end
