b5122cea1ae8e6a3c218c42ede03cdc13f08fe5e
metal-crow <frankcrisanti@aol.com>
2021-01-29 22:52:04 -0500

    version bump

--- DaS-PC-MPChan/MainWindow.Designer.vb
+++ DaS-PC-MPChan/MainWindow.Designer.vb
@@ -330,7 +330,7 @@ Partial Class MainWindow
         Me.lblVer.Name = "lblVer"
         Me.lblVer.Size = New System.Drawing.Size(76, 13)
         Me.lblVer.TabIndex = 49
-        Me.lblVer.Text = "2020.01.29.0"
+        Me.lblVer.Text = "2020.01.29.1"
         '
         'chkExpand
         '
